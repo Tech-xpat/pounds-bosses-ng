d396f6e3f66e2b91101deef620f179ffd997aaf4
Tech-xpat <empiredigitalsworldwide@gmail.com>
2025-03-17 14:28:37 +0100

    Delete the-pounds-bosses directory

--- the-pounds-bosses/components/mobile-nav.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const MobileNav: React.FC = () => {
-  return (
-    <nav>Mobile Navigation</nav>
-  );
-};
-
-export default MobileNav;
--- the-pounds-bosses/components/mode-toggle.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ModeToggle: React.FC = () => {
-  return (
-    <button>Toggle Mode</button>
-  );
-};
-
-export default ModeToggle;
